76af0768414241d22a3c8e232bfcd6d604b14f1a
Perkles <gabriel_felipe_trabalho@hotmail.com>
2023-01-25 15:22:05 -0300

    commit 1

--- Meu Primeiro Projeto Java/src/App.java
+++ Meu Primeiro Projeto Java/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Boas Vindas, Galera do Time da Turma DIO!");
+        System.out.println("Primeira Integração Minha entre o Java e o GIT");
     }
 }
